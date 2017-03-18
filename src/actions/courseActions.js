66a62a17cccd2580c5973145b20af20c9450779e
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-03-18 15:47:04 +0100

    Course Reducer + Create Action

---
+++ src/actions/courseActions.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Ibrahim on 18/03/2017.
+ */
+export function createCourse(course) {
+  return {type: 'CREATE_COURSE', course}
+}
---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Ibrahim on 18/03/2017.
+ */
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case
+    'CREATE_COURSE'
+    :
+      return [...state,
+        Object.assign({},action.course)
+      ];
+
+    default:
+      return state;
+  }
+}
